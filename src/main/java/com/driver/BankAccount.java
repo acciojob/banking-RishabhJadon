1c039d8a668e6a3ec8db3bb411836d82604d7710
Rishabh Singh <rishabhjadon976@gmail.com>
2023-11-05 14:02:41 +0530

    first commit

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -3,29 +3,68 @@ package com.driver;
 public class BankAccount {
 
     private String name;
+
+    public double getBalance() {
+        return balance;
+    }
+
     private double balance;
     private double minBalance;
 
     public BankAccount(String name, double balance, double minBalance) {
-
+        this.name = name;
+        this.balance = balance;
+        this.minBalance = minBalance;
     }
 
     public String generateAccountNumber(int digits, int sum) throws Exception{
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
-
-        return null;
+        StringBuilder accountNo = new StringBuilder();
+        int j = (int)Math.ceil((double) sum/digits);
+        if(j<=9)
+        {
+            int i = j;
+            while(sum>=0 && digits>0)
+            {
+               if(i>9)
+               {
+                   i = j;
+               }
+               if(sum<i) {
+                   i = sum;
+               }
+               accountNo.append(i);
+               sum -= i;
+               ++i;
+               --digits;
+            }
+            return accountNo.toString();
+        }
+        else {
+            throw new Exception("Account Number can not be generated");
+        }
     }
 
     public void deposit(double amount) {
         //add amount to balance
-
+        balance += amount;
     }
 
     public void withdraw(double amount) throws Exception {
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
-
+        try
+        {
+           if(balance-amount>=minBalance)
+           {
+             balance -= amount;
+           }
+        }
+        catch (Exception e)
+        {
+           System.out.println("Insufficient Balance");
+        }
     }
 
 }
--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -5,7 +5,13 @@ public class CurrentAccount extends BankAccount{
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
+        super(name,balance,5000);
+        this.tradeLicenseId = tradeLicenseId;
 
+        if(balance<5000)
+        {
+            throw new Exception("Insufficient Balance");
+        }
     }
 
     public void validateLicenseId() throws Exception {
@@ -13,7 +19,100 @@ public class CurrentAccount extends BankAccount{
         // If the license Id is valid, do nothing
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
-
+        if(!valid(tradeLicenseId))
+        {
+            String rearrangedString = rearrangeString(tradeLicenseId);
+            if(rearrangedString.equals(""))
+            {
+                throw new Exception("Valid License can not be generated");
+            }
+            else {
+                tradeLicenseId = rearrangedString;
+            }
+        }
     }
+
+    boolean valid(String str)
+    {
+        for(int i=0;i<str.length()-1;i++)
+        {
+            if(str.charAt(i)==str.charAt(i+1))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static String rearrangeString(String str)
+    {
+        int n = str.length();
+
+        if(n==0)
+        {
+            return "";
+        }
+
+        int[] arr = new int[26];
+
+        for(int i=0;i<26;i++)
+        {
+            arr[i] = 0;
+        }
+        for(char ch : str.toCharArray())
+        {
+            arr[ch-'A']++;
+        }
+
+        char ch_max = getMaxCountChar(arr);
+        int maxCount = arr[ch_max-'A'];
+
+        //check if result is possible or not
+        if(maxCount>(n+1)/2)
+        {
+            return "";
+        }
+
+        StringBuilder res = new StringBuilder();
+        res.append(" ".repeat(n));
+
+        int ind=0;
+        //filling the most frequently occuring char at even indices
+        while(maxCount-->0)
+        {
+            res = new StringBuilder(str.substring(0,ind) + ch_max + str.substring(ind+1));
+            ind+=2;
+        }
+        arr[ch_max-'A']=0;
+
+        //now filling others characters first fill even characters than odd characters
+        for(int i=0;i<n;i++)
+        {
+           while(arr[i]-->0)
+           {
+             ind = (ind>=n)?1:ind;
+             res = new StringBuilder(str.substring(0,ind) + (char)('A'+i) + str.substring(ind+1));
+             ind +=2;
+           }
+        }
+
+        return res.toString();
+    }
+
+   public static char getMaxCountChar(int arr[])
+   {
+       int max = 0;
+       char ch = ' ';
+
+       for(int i=0;i<arr.length;i++)
+       {
+           if(arr[i]>max)
+           {
+               max = arr[i];
+               ch = (char)(i+'A');
+           }
+       }
+       return ch;
+   }
 
 }
